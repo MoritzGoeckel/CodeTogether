9c2341ea7051c6f69fc49706f63c26c46b3958af
Moritz Goeckel <moritz.goeckel@gmail.com>
2022-11-27 01:17:42 +0100

    Using CodeJar

---
+++ frontend_server/frontend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use(express.static('static'))
+//app.use('/scripts', express.static('public'))
+
+/*app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});*/
