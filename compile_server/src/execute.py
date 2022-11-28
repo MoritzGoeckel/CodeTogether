91da09eea50b3777a2dc038741cdab5f16ab7264
Moritz Goeckel <moritz.goeckel@gmail.com>
2022-11-28 01:10:27 +0100

    Add python support to compile server

--- compile_server/src/execute.py
+++ compile_server/src/execute.py
@@ -1,18 +1,29 @@
 from shared import write, run, COMPILE_DIR
+import time
 import os
 
+def getFileName(extension):
+    secondsSinceEpoch = time.time()
+    return COMPILE_DIR + "/" + str(secondsSinceEpoch) + "." + extension
+
 def execute_js(content):
-    filename = COMPILE_DIR + "/test.js"
+    filename = getFileName("js")
     write(filename, content)
     result = run('sudo -u run_user node ' + filename)
     os.remove(filename)
     return result
 
 def execute_cpp(content):
+    # sudo apt-get install build-essential
+    # g++
     return execute_js(content)
 
 def execute_py(content):
-    return execute_js(content)
+    filename = getFileName("py")
+    write(filename, content)
+    result = run('sudo -u run_user python ' + filename)
+    os.remove(filename)
+    return result
 
 def execute(content, lang):
     if lang == 'js':
--- compile_server/test.sh
+++ compile_server/test.sh
@@ -1,3 +1,5 @@
 curl localhost:3457 -d '{"lang": "js", "code": "console.log(\"Aurelia\")"}' -H 'Content-Type: application/json'
 
 curl localhost:3457 -d '{"lang": "js", "code": "while(true){console.log(\"Aurelia\")}"}' -H 'Content-Type: application/json'
+
+curl localhost:3457 -d '{"lang": "py", "code": "print(\"Hello python!\")"}' -H 'Content-Type: application/json'
